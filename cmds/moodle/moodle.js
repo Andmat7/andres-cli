4fb27d36871d7296d6a23ea2f378db2f1da396b5
Andrés Corredor <andrescorredor20@gmail.com>
2019-05-28 18:34:35 -0500

    upload h5ps

--- cmds/moodle/moodle.js
+++ cmds/moodle/moodle.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 module.exports =
     class Moodle {
         constructor(args) {
@@ -5,13 +6,12 @@ module.exports =
             this.puppeteer = require('puppeteer');
             if (args.moodle_url) {
                 this.moodle_url = args.moodle_url
-            }else{
+            } else {
                 this.moodle_url = this.config.moodle.url
-
             }
             if (args.moodle_user) {
                 this.moodle_user = args.moodle_user
-            }else{
+            } else {
                 this.moodle_user = this.config.moodle.adminuser
 
             }
@@ -24,7 +24,18 @@ module.exports =
         }
         async _init() {
             this.browser = await this.puppeteer.launch({ devtools: this.devtools });
+            this.courses_path = process.cwd() + '/courses-lock.json'
+            if (fs.existsSync(this.courses_path)) {
+                this.courses = require(this.courses_path);
+            } else {
+                this.courses = {}
+            }
             this.page = await this.browser.newPage();
+        }
+        async _end() {
+            fs.writeFileSync(this.courses_path, JSON.stringify(this.courses, null, 4));
+            this.browser.close();
+            console.log("finish")
         }
         async _debugger() {
             await Promise.all([
@@ -67,7 +78,36 @@ module.exports =
             let course_id = await this._add_course(category_id, course_name, course_short_name)
             console.log("course created " + course_id)
             await this.browser.close();
+            await this._end()
             console.log('finish')
+        }
+
+        async _add_hvps(course, Lesson_level, Lesson_name, fromSCORM) {
+            if (!("sections" in course)) {
+                let sections = []
+                if (course.fromSCORM) {
+                    sections = require('./templates/course-sections-scorm.json');
+                }
+                course.sections = sections;
+            }
+            for (let section_id = 0; section_id < course.sections.length; section_id++) {
+                const section = course.sections[section_id];
+                for (let j = 0; j < section.activities.length; j++) {
+                    const activity = section.activities[j];
+                    if (!activity.id) {
+                        activity.id = await this._upload_hvp_with_type(activity.type_h5p, course.id, section_id, Lesson_level, Lesson_name);
+                    }
+                    section.activities[j] = activity
+                }
+                course.sections[section_id] = section
+
+            }
+
+            return course;
+        }
+        async _upload_hvp_with_type(type, course_id, section_id, Lesson_level, Lesson_name) {
+            let h5p_name = Lesson_level + 'Lesson' + Lesson_name + type + type + '.h5p'
+            await this._upload_hvp(course_id, section_id, 'UP/English/General/' + Lesson_level + '/Lesson' + Lesson_name + '/' + type + '/' + h5p_name)
         }
         async add_course_with_hvps(args) {
             await this._init()
@@ -75,18 +115,20 @@ module.exports =
             let category_id = args.category_id
             let course_name = args.course_name
             let course_short_name = args.course_short_name
-            let course_id = await this._add_course(category_id, course_name, course_short_name)
-            await Promise.all([
-                this._upload_hvp(course_id, 0, 'UP/English/General/A1/Lesson1/LearnAndDiscover/A1Lesson1LearnAndDiscoverLearnAndDiscover.h5p'),
-                this._upload_hvp(course_id, 0, 'UP/English/General/A1/Lesson1/ListeningPractice/A1Lesson1ListeningPracticeListeningPractice'),
-                this._upload_hvp(course_id, 0, 'UP/English/General/A1/Lesson1/Presentation/A1Lesson1PresentationPresentation.h5p'),
-                this._upload_hvp(course_id, 0, 'UP/English/General/A1/Lesson1/ReadAndWrite/A1Lesson1ReadAndWriteReadAndWrite.h5p'),
-                this._upload_hvp(course_id, 0, 'UP/English/General/A1/Lesson1/Vocabularies/A1Lesson1ConversationVocabulary.h5p')
-
-            ]);
-
-            this.browser.close();
-            console.log('finish')
+            let course_id;
+            if (!(course_short_name in this.courses)) {
+                course_id = await this._add_course(category_id, course_name, course_short_name)
+                this.courses[course_short_name] = {
+                    "id": course_id,
+                    "short_name": course_short_name,
+                    "name": course_name,
+                    "fromSCORM": true,
+                };
+            } else {
+                course_id = this.courses[course_short_name].id;
+            }
+            this.courses[course_short_name] = await this._add_hvps(this.courses[course_short_name], "A1", 1, true)
+            await this._end()
         }
 
         async delete_course(args) {
@@ -128,7 +170,9 @@ module.exports =
             await this.browser.close();
             console.log('finish')
         }
+
         async _upload_hvp(course_id, section, file) {
+
             try {
 
                 let url_upload = this.moodle_url + "course/modedit.php?add=hvp&type=&course=" + course_id + "&section=" + section + "&return=0&sr=0"
@@ -141,22 +185,27 @@ module.exports =
                 await input.uploadFile(file)
                 await frame.click('#upload')
                 await frame.click('#tab-panel-upload > div > div.upload-form > button')
-                await page.evaluate(
-                    () => {
-                        debugger
-                    }
-                )
-                await frame.waitForSelector('body > div > div.h5peditor-form')
+                await frame.waitForSelector('body > div > div.h5peditor-form', {
+                    timeout: 100000
+                })
                 await Promise.all([
                     page.evaluate(
                         () => {
-                            document.querySelector("#id_submitbutton2").click()
+                            document.querySelector("#id_submitbutton").click()
                         }
                     ),
                     page.waitForNavigation(),
                 ]);
+                let activity_string = await page.evaluate(
+                    () => {
+                        return document.querySelector("#section-0 .content ul.section>li:nth-last-child(2)").id
+                    }
+                )
+                let activity_id = activity_string.split('-')[1]
+                console.log('uploaded h5p with id' + activity_id)
+                return activity_id;
             } catch (error) {
-                console.log(error)
+                console.log(error.stack)
             }
 
         }
